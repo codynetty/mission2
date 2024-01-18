2e093e60d4b9c0ccad04816f99174061fbc8b188
codynetty <codynetty@CODYNETTESH324F>
2024-01-17 20:20:44 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace mission1
+{
+    class rolldice //make a class called rolldice
+    {
+        public int[] Rolling(int numRolls) // rolls two dices 
+        {
+            int[] Rolled = new int[13]; // creates the array with 13 elements
+
+            // for loop going through the amount of the times the user wants to roll the dice
+            for (int iCount = 0; iCount < numRolls; iCount++)
+            {
+                // random number of the first roll
+                int randNum1 = Program.random.Next(1, 7);
+
+                // random number of the second roll
+                int randNum2 = Program.random.Next(1, 7);
+
+                // adds the two rolls together
+                int sum = randNum1 + randNum2;
+
+                Rolled[sum]++; // adds the array to the 
+            }
+            // returns the rolled array
+            return Rolled;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Xml.Linq;
+using mission1;
+// Cody Nettesheim 
+// Section 001 
+
+internal class Program
+{
+    internal static Random random = new Random();
+
+    public static void Main(string[] args)
+    {
+        int numRolls = 0; // set the variable 
+
+        //Welcome lines to the game
+        Console.WriteLine("Welcome to the dice rolling game!");
+
+        Console.WriteLine("How many times do you want to roll? ");
+
+        // set the inputted by the user to numRolls
+        numRolls = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Total Number of Rolls = " + numRolls);
+
+
+        // Create an instance of the rolldice class and call the rolling method
+        rolldice diceRoller = new rolldice();
+
+        // Call the Rolling method using the instance
+        int[] results = diceRoller.Rolling(numRolls);
+
+        // call the histo function to make a histogram
+        Histo(results, numRolls);
+
+    }
+
+    // histogram function
+    public static void Histo(int[] Rolled, int numRolls)
+    {
+        // writing some info/instructions for the user
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+
+        // for loop to count the percentages on each time a number was rolled
+        for (int iCount = 2; iCount <= 12; iCount++)
+        {
+            // defines percentage and makes a percentage out of each array number 
+            int percentage = Rolled[iCount] * 100 / numRolls;
+
+            // repeats the * the same aomount of times it's equal to the percentage
+            string stars = new string('*', percentage);
+            // prints out for the user
+            Console.WriteLine($"{iCount}: {stars}");
+        }
+
+        Console.WriteLine("Thank for playing!");
+    }
+}
